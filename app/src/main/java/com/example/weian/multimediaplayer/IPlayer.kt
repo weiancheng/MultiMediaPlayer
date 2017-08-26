c86ff84004941075df344a1259112ce42d701f57
Weian <d92434101@gmail.com>
2017-08-27 00:30:28 +0800

    add media player files.

---
+++ app/src/main/java/com/example/weian/multimediaplayer/IPlayer.kt
@@ -0,0 +1,10 @@
+package com.example.weian.multimediaplayer
+
+/**
+ * an interface for controller multimedia player
+ *
+ * Created by weian on 2017/8/27.
+ */
+
+abstract class IPlayer {
+}
---
+++ app/src/main/java/com/example/weian/multimediaplayer/MediaPlayerProxy.kt
@@ -0,0 +1,7 @@
+package com.example.weian.multimediaplayer
+
+/**
+ * for controlling MediaPlayer
+ *
+ * Created by weian on 2017/8/27.
+ */
