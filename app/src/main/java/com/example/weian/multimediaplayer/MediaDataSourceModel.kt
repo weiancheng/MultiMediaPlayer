9274c078f3c15f3d36ab0a87d1eb5e1c153e49ce
Weian <d92434101@gmail.com>
2017-08-27 22:47:12 +0800

    create a media data source file

---
+++ app/src/main/java/com/example/weian/multimediaplayer/MediaDataSourceModel.kt
@@ -0,0 +1,10 @@
+package com.example.weian.multimediaplayer
+
+/**
+ * for storing media files
+ *
+ * Created by weian on 2017/8/27.
+ */
+
+class MediaDataSourceModel {
+}
--- app/src/main/java/com/example/weian/multimediaplayer/MediaPlayerProxy.kt
+++ app/src/main/java/com/example/weian/multimediaplayer/MediaPlayerProxy.kt
@@ -1,7 +1,40 @@
 package com.example.weian.multimediaplayer
 
+import android.media.MediaPlayer
+
 /**
  * for controlling MediaPlayer
  *
  * Created by weian on 2017/8/27.
  */
+
+class MediaPlayerProxy: IPlayer {
+    private val mMediaPlayer = MediaPlayer()
+
+    override fun play(uri: String) {
+    }
+
+    override fun stop() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun pause() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun resume() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun isPlaying() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun current() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun duration() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
